78e24abbb11434166b63c6abc900b62cb92e80ba
thidspessoa <thiagopessoa246@gmail.com>
2023-07-30 19:32:49 -0300

    Semana 1 - (24 a 28/07)

--- HTML, CSS - INTERFACE/ATIVIDADES/AULA 01/script.js
+++ HTML, CSS - INTERFACE/ATIVIDADES/AULA 01/script.js
@@ -1,14 +1,17 @@
-const openModallButton = document.querySelector(".opnModal");
-const closeModallButton = document.querySelector("#closeModal");
-const modal = document.querySelector(".modal");
-const fade = document.querySelector(".fade");
+const buttons = document.getElementsByClassName("opnModal");
+console.log(buttons)
+const [btn1, btn2, btn3, btn4, btn5] = buttons;
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
 
-const toggleModall= () => {
-    modal.classList.toggle("hide");
-    fade.classList.toggle("hide");
+//Função de callBack para disparar apos click
+const toggleModal = () => {
+    [modal, fade].forEach((el) => el.classList.toggle("hide"));
 }
 
-
-[openModallButton, closeModallButton, fade].forEach((el) => {
-    el.addEventListener("click", () => toggleModall());
+//Ouvinte no elemento
+[btn1, btn2, btn3, btn4, btn5, closeModalButton, fade].forEach((el) => {
+    el.addEventListener("click", () => toggleModal());
 }); 
+
