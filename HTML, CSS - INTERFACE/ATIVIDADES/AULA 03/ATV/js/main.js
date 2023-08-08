83c8a9a0340456f21113084c0b1baf430c4752e9
thidspessoa <thiagopessoa246@gmail.com>
2023-08-07 23:11:31 -0300

    add

---
+++ HTML, CSS - INTERFACE/ATIVIDADES/AULA 03/ATV/js/main.js
@@ -0,0 +1,12 @@
+// Mostra layout de adicionais
+mostrarBloco = () => {
+    let a = document.querySelector('.addSecond').style;
+    a.display = 'block';
+    }
+
+
+//Atualiza o valor do prato caso o cliente adicione adicional
+alterarPreco = () => { 
+    const element = document.getElementById('valorAdicional');
+    console.log(element)
+}
