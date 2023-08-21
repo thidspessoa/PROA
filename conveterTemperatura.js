3e8942de780af8792106aa7617f07a6b93f354ea
Thiago Pessoa <104585635+thidspessoa@users.noreply.github.com>
2023-08-21 17:54:57 -0300

    Add files via upload

---
+++ conveterTemperatura.js
@@ -0,0 +1,26 @@
+const temperaturaCelsius = parseFloat(prompt("Digite a temperatura em Celsius: "))
+const unidade = prompt("Para qual unidade de temperatura você quer converter \n A) Fahrenheit \n B) Kelvin")
+
+
+
+// KELVIN = TEMPERATURACELSIUS + 273,15 (kelvin)
+
+
+
+
+
+
+
+
+
+function conveterTemperatura (temperaturaBase, unidade) {
+
+    if (unidade == 'Fa') 
+        
+
+    //converterF = (temperaturaBase * 9/5) + 32
+    console.log(`${temperaturaBase}º Graus Celsius \n quando convertido para Fahrenheit são: ${converterF}º Graus Fahrenheit's `)
+}
+
+
+conveterTemperatura(temperaturaCelsius)
